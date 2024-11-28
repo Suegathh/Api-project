be73fb8a6e50d3f919e2d1e8af8dc830f43d7810
Suegathh <suzzainnegathu@gmail.com>
2024-11-28 14:49:39 +0300

    name change

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer(){
    return(
     <div>
         <footer className="text-center bg-black text-white fixed bottom-0 w-full">
-        <p>2024 ReadRealm. All Rights Reserved. Made by Georgina Kabasa</p>
+        <p>2024 ReadRealm. All Rights Reserved. Made by Susan Gathu</p>
         <div>
             <a href="/home" className="mr-3">Home</a>
             <a href="/books" className="mr-3">Books</a>
